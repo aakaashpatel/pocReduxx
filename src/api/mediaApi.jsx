b05d5d3ed64a977bea7bab21eba046a3d4449315
Aakash Patel <akashpatel8818@gmail.com>
2026-02-17 21:23:52 +0530

    Passing the Authorization and Client-ID

--- src/api/mediaApi.jsx
+++ src/api/mediaApi.jsx
@@ -4,8 +4,8 @@ const UNSPLASH_KEY = import.meta.env.VITE_UNSPLASH_KEY
 
  export  function fetchPhotos(query , page = 1, per_page= 20){
   const res = axios.get('https://api.unsplash.com/search/photos', {
-    params : {query},
-    headers : {}
+    params : {query, page, per_page},
+    headers : {Authorization : `Client-ID ${UNSPLASH_KEY}`}
   })
   console.log(res)
 }
