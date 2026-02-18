4c259f447c5cbe32baa33dc898898ceb246aa2e1
Aakash Patel <akashpatel8818@gmail.com>
2026-02-18 22:29:27 +0530

    setting the search slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,3 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import searchReducer from './features/searchSlice'
 
-export const store = configureStore()
+export const store = configureStore({
+  reducer: {
+    search: searchReducer,
+  },
+});
